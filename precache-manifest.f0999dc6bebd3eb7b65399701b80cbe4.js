df48356aeef942df1acdc95759d1f41b2f929458
Mukund <mukundrssharma@gmail.com>
2022-07-05 23:48:38 +0530

    Updates

---
+++ precache-manifest.f0999dc6bebd3eb7b65399701b80cbe4.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1f565cce50dadfdb0c4a995e7d798b5",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "a6f07b8597acb3dc1650",
+    "url": "/todolist/static/js/2.a6f19205.chunk.js"
+  },
+  {
+    "revision": "82a5e31e385099ac7323",
+    "url": "/todolist/static/js/main.b5698d78.chunk.js"
+  },
+  {
+    "revision": "022599b997120acd5a48",
+    "url": "/todolist/static/js/runtime-main.85dc825d.js"
+  }
+]);
